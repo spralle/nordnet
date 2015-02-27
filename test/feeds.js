e91fd09fe097f3e5c593c22f7557c861043250e3
Andreas Karlsson <sprawl@onlajn.nu>
2015-02-27 21:35:24 +0100

    Fixed auth + started on feeds

--- test/feeds.js
+++ test/feeds.js
@@ -2,11 +2,15 @@ var should = require('should');
 var NN = require('..');
 var config = require('../config/config');
 
-describe('Auth', function() {
+describe('Feeds', function() {
 	describe('#login', function() {
-		it('should login successfully', function(done) {
+		it('should login to feed successfully', function(done) {
+			this.timeout(150000);
 			NN.connect(config.url, config.keyfile).then(function(session) {
-				done();
+				session.feeds('public').connect().then(function(feed){
+
+					done();
+				});
 			}).catch(function(err) {
 				done(err);
 			});
