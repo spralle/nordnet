6c0a22640d86cb2605357c96e3f65d002e5d4619
Andreas Karlsson <sprawl@onlajn.nu>
2015-03-01 23:22:44 +0100

    fixed test

---
+++ test/accounts.js
@@ -0,0 +1,19 @@
+var should = require('should');
+var _ = require('lodash');
+
+var NN = require('..');
+var config = require('../config/config');
+describe('Accounts', function() {
+	describe('#get all accounts', function() {
+		it('Returns a list of accounts that the user has access to', function(done) {
+			NN.connect(config.url, config.keyfile).then(function(session) {
+				return session.accounts();
+			}).then(function(accounts) {
+				accounts.length.should.not.equal(0);
+				done();
+			}).catch(function(err) {
+				done(err);
+			});
+		});
+	});
+});
--- test/auth.js
+++ test/auth.js
@@ -6,6 +6,7 @@ describe('Auth', function() {
 	describe('#login', function() {
 		it('should login successfully', function(done) {
 			NN.connect(config.url, config.keyfile).then(function(session) {
+				session.isConnected().should.equal(true);
 				done();
 			}).catch(function(err) {
 				done(err);
