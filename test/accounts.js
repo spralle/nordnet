578d80ae3baf974ffe3b5f91b9c04f00b95f344f
Andreas Karlsson <sprawl@onlajn.nu>
2015-03-02 22:11:23 +0100

    started to add account methods

--- test/accounts.js
+++ test/accounts.js
@@ -7,7 +7,7 @@ describe('Accounts', function() {
 	describe('#get all accounts', function() {
 		it('Returns a list of accounts that the user has access to', function(done) {
 			NN.connect(config.url, config.keyfile).then(function(session) {
-				return session.accounts();
+				return session.accounts.all();
 			}).then(function(accounts) {
 				accounts.length.should.not.equal(0);
 				done();
